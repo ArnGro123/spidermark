6f1149e3f1b59172930ea4e0fbbbca3e83d700d8
ArnGro123 <98963288+ArnGro123@users.noreply.github.com>
2022-11-08 21:40:02 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -40,22 +40,64 @@ raw_data = pd.read_csv("investor_data.csv", index_col=False)
 industry_sel = st.selectbox("Choose Your Industry:", ["", "Renewable Energy", "Technology"])
 
 if industry_sel != "":
-    country_sel = st.selectbox("Choose Your Country:", [""]+[i for i in raw_data["country_code"].unique()])
-    if country_sel != "":
-        region_list = [""]+[i for i in raw_data["region"].unique()]
-        region_sel = st.selectbox("Choose Your Region:", region_list)
-        if region_sel != "":
+    country_list = [i for i in raw_data["country_code"].unique()]
+    country_sel = st.selectbox("Choose Your Country:", pd.DataFrame([""]+country_list).sort_values(0))
 
-            investor_data = raw_data[raw_data["short_description"].str.contains(industry_sel)].head()
-            num_investments = pd.DataFrame([i for i in range(investor_data.shape[1])], columns=["# of Investments"])
-            total_investments = pd.DataFrame([i for i in range(investor_data.shape[1])], columns=["Total Invested"])
-            display_data = [investor_data["name"], investor_data["short_description"], num_investments, total_investments]
-            display_data = pd.concat(display_data, axis=1)
-            st.write("Investors You Might Be Interested In:")
-            for i in display_data.iloc[0:display_data.shape[0]-16,:].iterrows():
-                if i[1][0] != np.NaN:
-                    st.code(i[1], "java")
+    if country_sel != "":
+
+        region_list = raw_data.drop_duplicates(subset="region")
+        filtered_region_list = []
+        investor_data = []
+        for row in region_list.iterrows():
+            if row[1][14] == country_sel:
+                raw_investor = []
+                raw_investor.append(pd.DataFrame([row[1][1]]))
+                raw_investor.append(pd.DataFrame([row[1][15]]))
+                raw_investor.append(pd.DataFrame([row[1][13]]))
+                raw_investor = pd.concat(raw_investor, axis=1)
+                investor_data.append(raw_investor)
+                filtered_region_list.append(row[1][13])
                
+        filtered_region_list = pd.DataFrame(filtered_region_list)
+        filtered_region_list.sort_values(0)
+        
+        region_sel = st.selectbox("Choose Your Region:", filtered_region_list)
+        investor_data = pd.concat(investor_data)
+        investor_data.columns = ["name", "short_description", "region"]
+        
+        if industry_sel == "Technology":
+            industry_sel = ["tech", "technologies", "technology"]
+        elif industry_sel == "Renewable Energy":
+            industry_sel = ["renewable", "renewable energy", "sustainable"]
+
+        for row in investor_data.head().iterrows():
+            for keyword in industry_sel:
+                if keyword in row[1][1]:
+                    st.code(row, "java")
+            
+            
+
+        # display_data = investor_data[investor_data["short_description"]].str.contains(industry_sel).head()
+        # print(investor_data)
+
+        # for i in display_data.iloc[0:display_data.shape[0]-16,:].iterrows():
+        #     if i[1][0] != np.NaN:
+        #         st.code(i[1], "java")
+        #         st.table(i[1])
+        #         st.dataframe(i[1])
+
+        # investor_data = raw_data[raw_data["short_description"].str.contains(industry_sel)].head()
+        # num_investments = pd.DataFrame([i for i in range(investor_data.shape[1])], columns=["# of Investments"])
+        # total_investments = pd.DataFrame([i for i in range(investor_data.shape[1])], columns=["Total Invested"])
+        # display_data = [investor_data["name"], investor_data["short_description"], num_investments, total_investments]
+        # display_data = pd.concat(display_data, axis=1)
+        # st.write("Investors You Might Be Interested In:")
+        # for i in display_data.iloc[0:display_data.shape[0]-16,:].iterrows():
+        #     if i[1][0] != np.NaN:
+                # st.code(i[1], "java")
+                # st.table(i[1])
+                # st.dataframe(i[1])
+            
 
 
           
